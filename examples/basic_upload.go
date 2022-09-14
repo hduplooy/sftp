09e8e507745cf8eb335a97526e79e04e6ba134d4
Hannes du Plooy <hduplooy@users.noreply.github.com>
2022-09-14 22:09:22 +0200

    Add files via upload

---
+++ examples/basic_upload.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hduplooy/sftp"
+	"log"
+	"time"
+)
+
+func main() {
+	config := sftp.Config{
+		Username: "root",
+		Password: "password", // required only if password authentication is to be used
+		Server:   "localhost:22",
+		Timeout:  time.Second * 30, // 0 for not timeout
+	}
+	client, err := sftp.New(config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	uploaded, err := client.UploadIfNewer("/home/hannes/testfile", "testfile")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Uploaded file: %v\n", uploaded)
+}
